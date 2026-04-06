4fdaac50cc25ed424ef1d4ffbf4dd0f50fa8bfc0
VasavMakwana <vasavmakwana@gmail.com>
2026-04-06 13:14:58 +0530

    Labwork 3.3

---
+++ Input numbers until user enters.py
@@ -0,0 +1,8 @@
+while True:
+    num = int(input("Enter a number (0 to stop): "))
+    if num == 0:
+        print("Program stopped")
+        break
+    else:
+        print("You entered:", num)
+        
---
+++ Multiples of 5 from 5 to 50.py
@@ -0,0 +1,2 @@
+for i in range(5, 61, 5):
+    print(i)
