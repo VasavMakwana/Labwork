4dcba3287a4ce236d48c71571ef6eb8ebf8a0181
VasavMakwana <vasavmakwana@gmail.com>
2026-04-06 13:03:08 +0530

    Labwork 3.1

---
+++ Age Group Categorization.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+
+if age >= 0 and age <= 12:
+    print("Child")
+elif age >= 13 and age <= 19:
+    print("Teenager")
+elif age >= 20 and age <= 59:
+    print("Adult")
+elif age >= 60:
+    print("Senior")
+else:
+    print("Invalid age")
+
+    
---
+++ Check Neutral Number (Zero).py
@@ -0,0 +1,8 @@
+num = int(input("enter a number:"))
+
+if num == 0:
+    print("it is a neutral number")
+elif num > 0:
+    print("it is a positive number")
+else:
+    print("it is a negative number")
